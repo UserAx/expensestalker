b9048db720498bd0cc88803c9e2d332b380f883b
UserAx <su.userax@gmail.com>
2020-03-17 14:31:49 +0545

    Changes to server.js

--- server/server.js
+++ server/server.js
@@ -3,12 +3,11 @@ const path = require('path');
 const app = express();
 const publicPath =  path.join(__dirname, '..', 'public');
 app.use(express.static(publicPath));
-const port = 3000 || process.env.PORT;
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(publicPath, 'index.html'));
 })
 
-app.listen(port, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log('Server is up.');
 });
